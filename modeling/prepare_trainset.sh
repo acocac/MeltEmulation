421b25060e4fbd3e232dcb071f8af255f5d46db1
acocac <acocac@gmail.com>
2026-07-03 10:12:08 +0100

    add bash

---
+++ modeling/prepare_trainset.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+#SBATCH -A aria_giant
+#SBATCH -J giant_poc-cpu
+#SBATCH -o .logs/giant-cpu-%j.out
+#SBATCH -e .logs/giant-cpu-%j.err
+#SBATCH -t 04:00:00 #15:30:00
+#SBATCH --partition=standard
+#SBATCH --qos=high
+#SBATCH --mem=256GB
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
+
+# activate the environment
+source $HOME/repos/giant/MeltEmulation/.venv/bin/activate
+
+srun uv run python modeling/prepare_trainset.py
