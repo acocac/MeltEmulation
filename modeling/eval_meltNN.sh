0d678fb06c842d011876671bd197121d64929f9d
acocac <acocac@gmail.com>
2026-07-09 12:34:29 +0100

    reduce time/storage eval

--- modeling/eval_meltNN.sh
+++ modeling/eval_meltNN.sh
@@ -6,13 +6,13 @@
 #SBATCH --mail-type=begin,end,fail,requeue
 #SBATCH --mail-user=acoca@turing.ac.uk
 #SBATCH --gres=gpu:1
-#SBATCH -t 20:00:00 #15:30:00
+#SBATCH -t 01:00:00 #15:30:00
 #SBATCH --partition=orchid
 #SBATCH --qos=orchid
 #SBATCH --gpus-per-node=1
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=3
-#SBATCH --mem=60GB
+#SBATCH --mem=8GB
 
 # environment variables
 export PATH=/usr/local/cuda-12.8/bin${PATH:+:${PATH}}
